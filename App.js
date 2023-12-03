705f652cc45dacf8dacfbcd063ba34c7d74e5282
Aditi Mittal <91279248+aditim26@users.noreply.github.com>
2023-12-03 20:59:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+// React Admin
+import { Admin, Resource } from "react-admin";
+
+// Provider
+import restProvider from "ra-data-simple-rest";
+
+// Components
+import { UserCreate, UserList, UserEdit } from "./components/Users";
+
+// App
+function App() {
+  return (
+    <Admin dataProvider={restProvider("http://localhost:3000")}>
+      <Resource
+        name="users"
+        list={UserList}
+        create={UserCreate}
+        edit={UserEdit}
+      />
+    </Admin>
+  );
+}
+
+export default App;
